69a9469bb5626192c8eb6ccd52e616e5e45fbfdc
afaq-bin-naeem <afaqbinnaeem@gmail.com>
2025-12-10 19:32:02 +0500

    Update contact_form_mailer.rb

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -2,7 +2,7 @@ class ContactFormMailer < ApplicationMailer
     def submit_contact_form(form)
 		@form = form
 		mail(
-			to: "atg2479@gmail.com",
+			to: "ahmad@astechware.com",
 			subject: 'You Have A Query Pending at astechware.com',
 			reply_to: @form.email # <-- safe way to use user's email
 		)
